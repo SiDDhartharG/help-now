cc30f32701182a4272a8f0fffe28d6c5f35342eb
Siddharth Garg <garg.sid6665@gmail.com>
2021-11-22 22:12:47 +0530

    Make child components

---
+++ src/components/header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import MedicalServicesIcon from '@mui/icons-material/MedicalServices';
+
+const Header = () => {
+    return <div>
+        <MedicalServicesIcon />Doctor e-clinics
+    </div>
+}
+
+export default Header
--- src/components/home-screen.js
+++ src/components/home-screen.js
@@ -1,19 +1,20 @@
 import React from 'react';
 import MainBodyBackgroundImage from '../utils/mainBodyBackground.jpg'
-import MedicalServicesIcon from '@mui/icons-material/MedicalServices';
-import { Grid } from '@mui/material';
+import Header from './header';
+import SideBar from './side-bar';
+import ChatBody from './chat-body';
 
 const HomeScreen = () => {
     return <div style={{ height: "inherit", display: "flex", flexDirection: "column" }}>
         <div style={{ height: "60px", background: "#59ebc4" }}>
-            <MedicalServicesIcon />  Header
+            <Header />
         </div>
         <div style={{ display: "flex", flexGrow: 10 }}>
-            <div style={{ borderRight: "2px solid #babfbe", width: "20%", overflow: "scroll" }}>
-                SideBar
+            <div style={{ borderRight: "2px solid #babfbe", width: "20%", overflowY: "hidden" }}>
+                <SideBar />
             </div>
-            <div style={{ backgroundImage: `url(${MainBodyBackgroundImage})`, width: "80%", overflow: "scroll" }}>
-                Body
+            <div style={{ backgroundImage: `url(${MainBodyBackgroundImage})`, width: "80%", overflowY: "hidden" }}>
+                <ChatBody />
             </div>
         </div>
     </div >
