578316c068b9b80c745892cf1bf3a9f8510b419a
Siddharth Garg <garg.sid6665@gmail.com>
2021-11-24 14:12:46 +0530

    Some css refinement

--- src/components/header.js
+++ src/components/header.js
@@ -2,8 +2,8 @@ import React from 'react';
 import MedicalServicesIcon from '@mui/icons-material/MedicalServices';
 
 const Header = () => {
-    return <div style={{ height: "60px", background: "#59ebc4", position: "fixed", top: 0, right: 0, left: 0, padding: "0px 16px", display: "flex", alignItems: "center" }}>
-        <MedicalServicesIcon />Doctor e-clinics
+    return <div style={{ height: "60px", color: "white", fontSize: 23, fontWeight: 600, background: "#59ebc4", position: "fixed", top: 0, right: 0, left: 0, padding: "0px 16px", display: "flex", alignItems: "center" }}>
+        <MedicalServicesIcon /> &nbsp; &nbsp;Doctor e-clinics
     </div>
 }
 
